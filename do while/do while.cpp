6a750e0ef37e7340c70c4c515d12b48dad6a8cf2
mirze <mirze@DESKTOP-H5NKI9E>
2024-08-19 19:29:43 +0400

    Add project files.

---
+++ do while/do while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //double usdAmount, aznAmount;
+    //const double usdToAzn = 1.70;  
+
+    //cout << "Məbləği USD olaraq daxil edin: ";
+    //cin >> usdAmount;
+
+    //aznAmount = usdAmount * usdToAzn;
+
+    //cout << usdAmount << " USD -> " << aznAmount << " AZN" << endl;
+
+
+}
